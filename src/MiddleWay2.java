12e26155fbdd4c928234416c3536af37d0e68dbd
Jan Gevaert <jan.gevaert@gmail.com>
2020-10-27 05:21:44 -0600

    Initial commit

---
+++ src/MiddleWay2.java
@@ -0,0 +1,11 @@
+public class MiddleWay2 {
+  public int[] middleWay(int[] a, int[] b) {
+    int[] result = new int[2];
+    result[0] = a[1];
+    result[1] = b[1];
+    return result;
+  }
+
+// int[] result = new int[] {a[1], b[1]};
+// return result;
+}
